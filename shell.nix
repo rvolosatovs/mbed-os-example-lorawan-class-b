3f84355af37ca9283ce99a8025d72ff70ad74074
Roman Volosatovs <rvolosatovs@riseup.net>
2019-12-03 00:36:05 +0100

    shell.nix: Use dockerImages.mbed

--- shell.nix
+++ shell.nix
@@ -2,20 +2,7 @@ with import <nixpkgs> {};
 stdenv.mkDerivation {
   name = "env";
   buildInputs = [
-    cargo
-    gcc-arm-embedded-6
     git
-    mercurial
-    nodejs
-    openssl
-    python3Full
-    python3Packages.virtualenv
+    dockerImages.mbed
   ];
-
-  shellHook = ''
-    # set SOURCE_DATE_EPOCH so that we can use python wheels
-    SOURCE_DATE_EPOCH=$(date +%s)
-    export PATH=$PWD/venv/bin:$PATH
-    virtualenv --no-setuptools venv >&2
-  '';
 }
